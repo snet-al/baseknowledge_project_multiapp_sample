53720af0842a4d1a258b151ec529ca3bdb611d06
snet <developer.snet.al@gmail.com>
2019-09-03 03:48:41 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/snet-al/app_laravel.git app_laravel
 git clone https://github.com/snet-al/app_sci.git app_sci
-git clone app_vue
-git clone app_react
+git clone https://github.com/snet-al/app_vue.git app_vue
+#git clone app_react
